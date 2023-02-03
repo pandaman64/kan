b8060c5e6e5e92526ae64c208eec578bf2d77d83
pandaman64 <kointosudesuyo@infoseek.jp>
2023-02-03 23:01:55 +0900

    Full height CodeMirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,51 @@
 import {
+  Box,
   Container,
   CssBaseline,
   CssVarsProvider,
   Grid,
+  Stack,
   Typography,
 } from "@mui/joy"
+import ReactCodeMirror from "@uiw/react-codemirror"
+import { EditorView } from "codemirror"
+import "./App.css"
+
+const theme = EditorView.theme({
+  "&": { height: "100%" },
+  ".cm-gutter,.cm-content": { height: "100%" },
+  ".cm-scroller": { overflow: "auto", height: "100%" },
+})
 
 function App() {
   return (
     <CssVarsProvider>
       <CssBaseline />
       <Container maxWidth="md">
-        <Grid container>
-          <Grid xs={4}>
-            <Typography level="h2">Input</Typography>
+        <Stack minHeight="100vh" gap={2}>
+          <Grid container>
+            <Grid xs={4}>
+              <Typography level="h2">Input</Typography>
+            </Grid>
+            <Grid xs={8}>
+              <Typography level="body1">Foobar</Typography>
+            </Grid>
+            <Grid xs={4}>
+              <Typography level="h2">Transform</Typography>
+            </Grid>
+            <Grid xs={8}>
+              <Typography level="body1">Foobar</Typography>
+            </Grid>
           </Grid>
-          <Grid xs={8}>
-            <Typography level="body1">Foobar</Typography>
-          </Grid>
-          <Grid xs={4}>
-            <Typography level="h2">Transform</Typography>
-          </Grid>
-          <Grid xs={8}>
-            <Typography level="body1">Foobar</Typography>
-          </Grid>
-        </Grid>
+          <Box flexGrow={1} position="relative" id="grow">
+            <Box position="absolute" id="abs" width="100%" height="100%">
+              <ReactCodeMirror
+                value="console.log('hello world!')"
+                theme={theme}
+              />
+            </Box>
+          </Box>
+        </Stack>
       </Container>
     </CssVarsProvider>
   )
